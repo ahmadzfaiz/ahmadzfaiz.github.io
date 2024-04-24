ba98e10366b8f5d56238f20b9d5aedaf2fe5e57d
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2024-04-24 21:56:53 +0700

    modified:   index.html 	new file:   main.js 	new file:   styles.css
    message: push ke server

---
+++ main.js
@@ -0,0 +1,51 @@
+// const PI = 3.14
+
+// let angka = 5
+
+// // console.log(PI)
+// // console.log(angka)
+
+// angka = 7
+
+// // PI = 4
+
+// // console.log(angka)
+
+// let nama = "Faiz"
+
+// // console.log(nama)
+
+// let perempuan = false
+
+// // console.log(perempuan)
+
+// // console.log(typeof(PI))
+// // console.log(typeof(angka))
+// // console.log(typeof(nama))
+// // console.log(typeof(perempuan))
+
+// let data = null
+
+// console.log(data)
+// console.log(typeof(data))
+
+// let variabel
+
+// console.log(variabel)
+// console.log(typeof(variabel))
+
+
+// FUNGSI
+// function f(x){
+//   return 5 + x
+// }
+
+// console.log(f(5))
+// console.log(f(2))
+
+// function f(x, y){
+//   return 10 / x + y
+// }
+
+// console.log(f(2,3))
+// console.log(f(5,3))
