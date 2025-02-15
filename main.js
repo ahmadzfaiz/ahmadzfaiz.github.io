e2b55c9bec041a894cc0cd203afab352cd7df235
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2025-02-15 21:55:09 +0700

    Deploy Webgis Leaflet
    Melakukan deployment webgis dengan menggunakan leaflet ke GitHub.
    Sabtu, 15 Februari 2025

--- main.js
+++ main.js
@@ -1,184 +1,115 @@
-// const PI = 3.14
+// 
 
-// let angka = 5
+const tampilanArea = L.latLngBounds(
+  [-8.41588881812499, 125.19748144811572]
+  [-11.33188525484632, 122.31844017304712],
+)
 
-// // console.log(PI)
-// // console.log(angka)
+const peta = L.map('peta', {
+  center: [
+    -10.168398085111578, 
+    123.60111216928792
+  ],
+  zoom: 12,
+  minZoom: 6,
+  maxZoom: 15,
+  // maxBound: tampilanArea // fungsi ini tidak berjalan di browser terbaru
+})
 
-// angka = 7
+L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors",
+    minZoom: 0,
+    maxZoom: 20
+  }
+).addTo(peta)
 
-// // PI = 4
+const pantaiNamosain = L.marker([
+  -10.17257953762943, 
+  123.56104213121834
+]).addTo(peta)
 
-// // console.log(angka)
+pantaiNamosain.bindPopup(`
+  <h4>Pantai Namosain</h4>
+  <img id="namosain" src="./media/pantai namosain.jpg" alt="Foto Pantai Namosain">
+`)
 
-// let nama = "Faiz"
+const garis = L.polyline([
+  [-10.195950123929919, 123.52703185051459],
+  [-10.182236097778729, 123.61336458842223],
+  [-10.143987670268743, 123.61684229163217]
+]).addTo(peta)
 
-// // console.log(nama)
+const area1 = L.polygon([
+  [-10.188820069028539, 123.5561547554247],
+  [-10.186259597868457, 123.58645868143866],
+  [-10.170899001383011, 123.57150165139018]
+]).addTo(peta)
 
-// let perempuan = false
-
-// // console.log(perempuan)
-
-// // console.log(typeof(PI))
-// // console.log(typeof(angka))
-// // console.log(typeof(nama))
-// // console.log(typeof(perempuan))
-
-// let data = null
-
-// console.log(data)
-// console.log(typeof(data))
-
-// let variabel
-
-// console.log(variabel)
-// console.log(typeof(variabel))
-
-
-// FUNGSI
-// function f(x){
-//   return 5 + x
-// }
-
-// console.log(f(5))
-// console.log(f(2))
-
-// function f(x, y){
-//   return 10 / x + y
-// }
-
-// console.log(f(2,3))
-// console.log(f(5,3))
-
-// LOGIKA CONDITIONAL
-// let nama = 'faiz'
-
-// nama = 'agus'
-
-// if(nama === 'faiz'){
-//   console.log('Hasilnya adalah WEBGIS')
-// } else if(nama === 'bambang'){
-//   console.log('Hasilnya adalah LEMARI')
-// } else{
-//   console.log('Hasilnya adalah TIDAK ADA')
-// }
-
-// const angka1 = 5
-// const angka2 = 2 + 3
-
-// console.log(angka1 > angka2)
+const area2 = L.polygon([
+  [
+    [-10.149007340116345, 123.54353887448549],
+    [-10.134153320921383, 123.61507165486397],
+    [-10.094723242905584, 123.55771629304508]
+  ],
+  [
+    [-10.122378336132286, 123.55511477998978],
+    [-10.113287766599598, 123.58125536863486],
+    [-10.132236512941851, 123.57748458157269]
+  ]
+]).addTo(peta)
 
 
-// LOOP
-// let percobaan = 0
-
-// if(percobaan < 3){
-//   console.log('Anda bisa login')
-//   percobaan = percobaan + 1
-//   console.log('percobaan ke-' + percobaan)
-// } else{
-//   console.log('Tunggu selama 15 menit')
-// }
-
-// while(percobaan < 10){
-//   console.log('Anda bisa login')
-//   percobaan = percobaan + 1
-//   console.log('percobaan ke-' + percobaan)
-// }
-
-// if(percobaan >= 10){
-//   console.log('Mohon tunggu selama 15 menit')
-// }
-
-// const nama = 'ENVIROSAINS'
-
-// for(let i=0; i<nama.length; i++){
-//   console.log(nama[i])
-// }
-
-// ARRAY & OBJECT
-
-// const siswa = ['faiz', 'bagus', 'zainal']
-// console.log(siswa[3])
-
-// const lokasi = [
-//   [2, 5],
-//   [4, 7],
-//   [6, 3]
-// ]
-
-// console.log(lokasi[1][0])
-
-// const rumah = {
-//   pemilik: 'faiz',
-//   harga: 500000000,
-//   lokasi: [100, 5]
-// }
-
-// console.log(rumah.lokasi[1])
-
-// ALGORITMA WEBGIS
-// Konfigurasi tampilan peta
-const settingPeta = {
-  center: [0, 110],
-  zoom: 5
-}
-const peta = L.map('peta', settingPeta)
-
-// Konfigurasi Basemap
-const urlBasemap = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
-const settingBasemap = {
-  attribution: 'OpenStreetMap'
-}
-const basemap = L.tileLayer(urlBasemap, settingBasemap)
-basemap.addTo(peta)
-
-const urlBasemap2 = 'http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
-const settingBasemap2 = {
-  attribution: 'ESRI'
-}
-const basemap2 = L.tileLayer(urlBasemap2, settingBasemap2)
-// basemap2.addTo(peta)
-
-// Konfigurasi Layer
-const balikpapan = L.marker([-1.246346253716088, 116.85033780476381])
-// balikpapan.addTo(peta)
-balikpapan.bindPopup('Balikpapan')
-
-const palembang = L.marker([-2.9693631451481286, 104.76914637884609])
-// palembang.addTo(peta)
-palembang.bindPopup('<img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-sBSCjoXYXyQ%2FUHF-KMkuGNI%2FAAAAAAAAAAk%2FXlrX316sK-E%2Fs1600%2Fpalembang.jpg&f=1&nofb=1&ipt=38d701471802481d6edfd0e010adf6ae1c8c3323960a89f4930bfa2510e94765&ipo=images" alt="" height="50">')
-
-const denpasar = L.marker([-8.65196333171677, 115.21568508602427])
-// denpasar.addTo(peta)
-denpasar.bindPopup('<a href="https://en.wikipedia.org/wiki/Denpasar">Kota Denpasar</a>')
-
-const settingPolygon = {
-  color: 'rgb(10, 215, 102)'
-}
-const polygon = L.polygon([
-  [-1.246346253716088, 116.85033780476381],
-  [-2.9693631451481286, 104.76914637884609],
-  [-8.65196333171677, 115.21568508602427]
-], settingPolygon)
-// polygon.addTo(peta)
-
-fetch('./data/lokasi.geojson')
+fetch("./data/objek.geojson")
   .then(response => response.json())
-  .then(json => L.geoJSON(json).addTo(peta))
+  .then(data => objek(data))
 
-// Konfigurasi Layer Group & Control
-const kota = L.layerGroup([balikpapan, palembang, denpasar])
+function objek(data){
+  const geojson = L.geoJSON(data)
+  geojson.addTo(peta)
 
-const toogleBasemap = {
-  'OSM basemap': basemap,
-  'ESRI NatGeo': basemap2
+  geojson.eachLayer(tampilanPopup)
 }
 
-const toogleLayer = {
-  kota: kota,
-  polygon: polygon
+function tampilanPopup(feature){
+  const nama = feature.feature.properties.nama
+  const foto = feature.feature.properties.foto
+
+  feature.bindPopup(`
+    <h4>${nama}</h4>
+    <img class="popup" src="${foto}">
+  `)
 }
 
-const layerControl = L.control.layers(toogleBasemap, toogleLayer)
-layerControl.addTo(peta)
+
+// fetch("./data/titik.geojson")
+//   .then(response => response.json())
+//   .then(data => L.geoJSON(data).addTo(peta))
+
+
+// const geojsonInput = {
+//   "type": "FeatureCollection",
+//   "name": "titik",
+//   "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+//   "features": [
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.556553302818941, -10.181978240354216 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.558556624799223, -10.198935030194676 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.577788515809885, -10.217073852363479 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.62707023652473, -10.223777068302535 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.614649640247009, -10.191836948876212 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.627470900920798, -10.166598046777143 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.658722723813128, -10.198935030194676 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.689974546705486, -10.169358648893262 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.665934682942122, -10.1500339333955 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.627871565316852, -10.149245144695872 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.644699469951178, -10.111775444851036 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.643497476763017, -10.076273802668888 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.60303037276141, -10.098364173673261 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.556152638422887, -10.08929152609122 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.54413270654122, -10.10783101140877 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.538122740600372, -10.141357150805787 ] } },
+//     { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 123.516486863213373, -10.128341536274293 ] } }
+//   ]
+// }
+
+// L.geoJSON(geojsonInput).addTo(peta)
